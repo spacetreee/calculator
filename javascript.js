91225ae0e20625cdaa96b0b4cfb7ad9837a5da7e
Emma <111027901+spacetreee@users.noreply.github.com>
2023-02-11 10:02:01 -0500

    Add eventlistener for buttons

--- javascript.js
+++ javascript.js
@@ -17,3 +17,15 @@ function divide(a, b) {
 function operate(operator, a, b) {
     return operator(a,b);
 }
+
+const buttons = document.querySelectorAll('button');
+const result = document.querySelector('.result');
+
+buttons.forEach(button => {
+    button.addEventListener('click', callback(button.id));
+})
+
+function callback(num) {
+    result.textContent = num;
+    console.log(num);
+}
