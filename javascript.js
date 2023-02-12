e2d797d05b4f540ec04462b9ea7c0400f1906e39
Emma <111027901+spacetreee@users.noreply.github.com>
2023-02-11 20:33:34 -0500

    Track operations in array

--- javascript.js
+++ javascript.js
@@ -20,17 +20,20 @@ function operate(operator, a, b) {
 
 const buttons = document.querySelectorAll('button');
 const result = document.querySelector('.result');
+const operations = document.querySelectorAll('button operator');
 let displayValue = 0;
+let firstNum = 0;
+const opArray = ['add', 'subtract', 'multiply', 'divide'];
 
-function callback(button) {
+function display(button) {
     displayValue += button.target.id;
-    console.log(displayValue);
     result.textContent = parseInt(displayValue);
     displayValue = parseInt(displayValue);
+    firstNum = displayValue;
     return displayValue;
 }
-console.log(displayValue);
+
 buttons.forEach(button => {
-    button.addEventListener('click', callback);
+    button.addEventListener('click', display);
 })
 
